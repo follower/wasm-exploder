0c81870a62090019e22a6f3e2349800ee7e9b150
follower <follower@rancidbacon.com>
2020-07-09 23:33:48 +1200

    Add script for WASM module display UI functionality.

---
+++ src/WasmModuleDisplayUI.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _ready():
+    pass
+
+
+# TODO: Pass loaded module instead?
+func display_exploded_wasm_module(wasm_file_path: String):
+
+    prints("Selected file:", wasm_file_path)
