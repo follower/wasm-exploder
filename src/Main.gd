940b73fe02f969d8e24b8ae1193a845c765388e8
follower <follower@rancidbacon.com>
2020-07-09 23:28:08 +1200

    Focus file open button on launch.

--- src/Main.gd
+++ src/Main.gd
@@ -2,9 +2,14 @@ extends Control
 
 onready var INPUT_WASM_FILE_PATH: LineEdit = self.find_node("WasmFilePath", true, false)
 
+onready var BUTTON_OPEN_WASM_FILE: Button = self.find_node("ButtonSelectWasmFile", true, false)
+
+
 func _ready():
 
     $"ColorRectBackground".color = $"ColorRectBackground".color.lightened(0.25)
+
+    BUTTON_OPEN_WASM_FILE.grab_focus()
 
 
 func _on_ButtonSelectWasmFile_pressed() -> void:
