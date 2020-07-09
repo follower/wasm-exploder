daa13a9aab12f238265c23336e97128dac53b304
follower <follower@rancidbacon.com>
2020-07-09 23:06:48 +1200

    Enable WASM file to be selected via dialog box.

--- src/Main.gd
+++ src/Main.gd
@@ -1,5 +1,15 @@
 extends Control
 
+onready var INPUT_WASM_FILE_PATH: LineEdit = self.find_node("WasmFilePath", true, false)
+
 func _ready():
 
     $"ColorRectBackground".color = $"ColorRectBackground".color.lightened(0.25)
+
+
+func _on_ButtonSelectWasmFile_pressed() -> void:
+    $"DialogSelectWasmFile".popup_centered_ratio()
+
+
+func _on_DialogSelectWasmFile_file_selected(path: String) -> void:
+    INPUT_WASM_FILE_PATH.text = path
