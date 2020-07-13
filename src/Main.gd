1a23f52616a4b62726c0d51af16919f019399353
follower <follower@rancidbacon.com>
2020-07-13 14:59:56 +1200

    Open About dialog when about link/button pressed.
    Note: The OK button doesn't seem to display the correct initial
          colour for some reason...

--- src/Main.gd
+++ src/Main.gd
@@ -27,3 +27,12 @@ func _on_DialogSelectWasmFile_about_to_show() -> void:
     # Workaround (bug?) where window title seems to get reverted to default "open" value.
 
     $"DialogSelectWasmFile".window_title = "Select WASM File..."
+
+
+func _on_LabelAboutLink_gui_input(event: InputEvent) -> void:
+
+    if event is InputEventMouseButton:
+        if event.pressed and event.button_index == BUTTON_LEFT:
+            get_tree().set_input_as_handled()
+            $"DialogAbout".popup_centered_ratio()
+            $"DialogAbout".grab_focus()
